02f88ba127726a3d5e6855091418dc7b6783a74f
Glitch (statuesque-lopsided-maiasaura) <none>
2020-08-13 02:53:48 +0000

    All test passing

--- public/sudoku-solver.js
+++ public/sudoku-solver.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
   answer = puzzlesAndSolutions[original][1];
   original = puzzlesAndSolutions[original][0];
   textArea.value = original;
-  addToGrid();
+  window.addToGrid();
 });
 
 /**
@@ -18,25 +18,60 @@ document.addEventListener('DOMContentLoaded', () => {
 	Verifies if the input is a valid number betwwen 1 and 9
 	@returns {boolean}
 */
-function isValidNumber(input){
+window.isValidNumber = (input) => {
 	return !isNaN(input) && Number(input) > 0 && Number(input) < 10;
 }
 
+
 /**
-	@method validateTextArea
+	@method findError
 	Verifies if the textArea's length is different form 81 
 	and if it posses different a character than "." or a number except 0.
 	If one of these conditions are true, then an error message will be shown
 */
-window.validateTextArea = () => {
+window.findError = () => {
 	let value = textArea.value;
-	let div = document.getElementById('error-msg');
-	div.innerHTML = "";
 	if(value.length != 81){
-		div.innerHTML = "Error: Expected puzzle to be 81 characters long.";
+		return "Error: Expected puzzle to be 81 characters long.";
 	}else if(!/^([1-9]|\.)+$/g.test(value)){
-		div.innerHTML = "Error: A non allowed character was detected.";
+		return "Error: A non allowed character was detected.";
+	}else {
+		return "";
 	}
+}
+
+/**
+	@method validateTextArea
+	Sets the error message to the div
+*/
+window.validateTextArea = () => {
+	let div = document.getElementById('error-msg');
+	div.innerHTML = window.findError();
+}
+
+/**
+  @method setInput
+  @param {string} input Optional puzzle string
+  Adds an input to the textArea
+*/
+window.setInput = (input) => {
+  textArea.value = input;
+  original = input;
+  for(let i = 0; i < puzzlesAndSolutions.length; i++){
+    if(puzzlesAndSolutions[i][0] == input){
+        answer = puzzlesAndSolutions[i][1];
+    }    
+  }
+  window.addToGrid();
+}
+
+/**
+  @method getGridPosition
+  @param {string} position Id of the grid to find
+  @returns {number} value on the position
+*/
+window.getGridPosition = (position) => {
+  return document.getElementById(position).value;
 }
 
 /**
@@ -44,8 +79,9 @@ window.validateTextArea = () => {
 	checks for changes in the textArea, adding the new numbers to the grid
 */
 window.addToGrid = () => {
-	let area = textArea.value;
-	for(let i = 0; i < area.length; i++){
+  window.validateTextArea();
+  let area = textArea.value;
+	for(let i = 0; i < 81; i++){
 		let position = Math.floor(i/9);
 		let matrix;
 		switch(position){
@@ -79,7 +115,7 @@ window.addToGrid = () => {
 		matrix += ((i % 9) + 1);
 		matrix = document.getElementById(matrix);
 		if(!isNaN(area[i]) && Number(area[i]) > 0){
-			matrix.value = area[i];
+			matrix.value = area[i] || "";
 		}else{
 			matrix.value = "";
 		}
@@ -95,12 +131,13 @@ window.addToGrid = () => {
 */
 window.validateNumber = (id, value) => {
 	let elem = document.getElementById(id);
-	if(isValidNumber(elem.value)){
-		let position = ((id[0].charCodeAt(0) - 65) * 9) + Number(id[1]) - 1;
-		textArea.value = textArea.value.substring(0, position) + value + textArea.value.substring(position + 1);
-	}else {
+  let position = ((id[0].charCodeAt(0) - 65) * 9) + Number(id[1]) - 1;
+	if(!window.isValidNumber(elem.value)){
 		elem.value = "";
+    value = ".";
 	}
+  textArea.value = textArea.value.substring(0, position) + value + textArea.value.substring(position + 1);
+  document.getElementById("text-input").value = textArea.value;
 }
 
 /**
@@ -109,7 +146,8 @@ window.validateNumber = (id, value) => {
 */
 window.clearButton = () => {
 	textArea.value = original;
- 	addToGrid();
+  document.getElementById("text-input").value = textArea.value;
+ 	window.addToGrid();
 }
 
 /**
@@ -118,7 +156,61 @@ window.clearButton = () => {
 */
 window.solveButton = () => {
 	textArea.value = answer;
- 	addToGrid();
+  document.getElementById("text-input").value = textArea.value;
+ 	window.addToGrid();
+}
+
+/**
+	@method checkSolution
+	@param {string} input A given solution
+  checks if the given solution is is right
+*/
+window.checkSolution = (input) => {
+  //check on each row
+  for(let i = 0; i < 81; i = i + 9){
+    let set = new Set();
+    for(let j = i; j < i + 9; j++){
+      set.add(input[j]);
+    }
+    if(set.size < 9){
+      return false;
+    }
+  }
+  //check on each column
+  for(let i = 0; i < 9; i++){
+    let set = new Set();
+    for(let j = i; j < 81; j = j + 9){
+      set.add(input[j]);
+    }
+    if(set.size < 9){
+      return false;
+    }
+  }
+  //check on each square
+  for(let i = 0; i < 81; i = i + 3){
+    let set = new Set([input[i], input[i+1], 
+      input[i+2], input[i+9], input[i+10], 
+      input[i+11], input[i+18], input[i+19], input[i+20]]);
+    if(set.size < 9){
+      return false;
+    }    
+    i = i == 6 ? 24 : (i == 33 ? 51 : (i == 60 ? 78: i));
+  }
+  return true;
+}
+
+/**
+	@method compare
+	@param {string} input A given solution
+  checks if the given input matches a solution
+*/
+window.compare = (input) => {
+  for(let i = 0; i < puzzlesAndSolutions.length; i++){
+   if(puzzlesAndSolutions[i][0] == input){
+     return puzzlesAndSolutions[i][1];
+   }
+  }
+  return false;
 }
 
 /* 
@@ -128,10 +220,17 @@ window.solveButton = () => {
 */
 try {
   module.exports = {
-  	validateTextArea: validateTextArea(),
-  	addToGrid: addToGrid(),
-  	validateNumber: validateNumber(id, value),
-  	clearButton: clearButton(),
-	solveButton: solveButton()
+    setInput: window.setInput,
+    getGridPosition: window.getGridPosition,
+    isValidNumber: window.isValidNumber,
+    findError: window.findError,
+  	validateTextArea: window.validateTextArea,
+  	addToGrid: window.addToGrid,
+  	validateNumber: window.validateNumber,
+    checkSolution: window.checkSolution,
+    compare: window.compare,
+  	clearButton: window.clearButton,
+  	solveButton: window.solveButton
   }
-} catch (e) {}
+} catch (e) {
+}
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -23,15 +23,21 @@ suite('Functional Tests', () => {
     // Entering a valid number in the text area populates 
     // the correct cell in the sudoku grid with that number
     test('Valid number in text area populates correct cell in grid', done => {
-
-      // done();
+      const input = '.7.89.....5....3.4.2..4..1.5689..472...6.....1.7.5.63873.1.2.8.6..47.1..2.9.387.6';
+      Solver.setInput(input);
+      assert.equal(document.getElementById("A2").value, "7");
+      done();
     });
 
     // Entering a valid number in the grid automatically updates
     // the puzzle string in the text area
     test('Valid number in grid updates the puzzle string in the text area', done => {
-
-      // done();
+      const input = '.7.89.....5....3.4.2..4..1.5689..472...6.....1.7.5.63873.1.2.8.6..47.1..2.9.387.6';
+      Solver.setInput(input);
+      document.getElementById("A1").value = "3";
+      Solver.validateNumber("A1", "3");
+      assert.equal(Number(document.getElementById('text-input').value[0]), 3);
+      done();
     });
   });
   
@@ -39,15 +45,24 @@ suite('Functional Tests', () => {
     // Pressing the "Clear" button clears the sudoku 
     // grid and the text area
     test('Function clearInput()', done => {
-
-      // done();
+      const base = '..839.7.575.....964..1.......16.29846.9.312.7..754.....62..5.78.8...3.2...492...1';
+      const solution = '218396745753284196496157832531672984649831257827549613962415378185763429374928561';
+      Solver.setInput(base);
+      document.getElementById("A1").value = "3";
+      Solver.clearButton();
+      assert.equal(document.getElementById('text-input').value, base);
+      done();
     });
     
     // Pressing the "Solve" button solves the puzzle and
     // fills in the grid with the solution
     test('Function showSolution(solve(input))', done => {
-
-      // done();
+      const base = '..839.7.575.....964..1.......16.29846.9.312.7..754.....62..5.78.8...3.2...492...1';
+      const solution = '218396745753284196496157832531672984649831257827549613962415378185763429374928561';
+      Solver.setInput(base);
+      Solver.solveButton();
+      assert.equal(document.getElementById('text-input').value, solution);
+      done();
     });
   });
 });
